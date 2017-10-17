7173785b47e4e37fad4b078ea3e9507eb158ec79
Daniel Walter <danielwalterpand@gmail.com>
2017-10-17 21:07:07 -0200

    First Interpolação e Boolean

--- Program.cs
+++ Program.cs
@@ -12,7 +12,16 @@ namespace console_dotnet
             Double valor = 70.7;
             Double quantidade =7.70;
             Double forgiveness = valor * quantidade;
-            Console.WriteLine(forgiveness);
+            Console.WriteLine($"seu cálculo: {valor} * {quantidade}; é igual a: " + forgiveness);
+
+            Boolean maiorQueDez = valor > 10;
+            if(maiorQueDez){
+                //1ºManeira de escrita
+                Console.WriteLine($"E adivinha... \n {forgiveness} é maior que dez!! :D");
+            }else{
+                //2°Maneira de escrita
+                Console.WriteLine("E adivinha... \n {0} é menor que dez!! D:", forgiveness);
+            }
  
         }
     }
